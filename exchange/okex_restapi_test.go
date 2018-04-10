3b794f2ddf5018a69e9451654ff9e2f4dc3e0937
maisuid <maisuid@gmail.com>
2018-04-10 16:59:49 +0800

    general update

--- exchange/okex_restapi_test.go
+++ exchange/okex_restapi_test.go
@@ -11,17 +11,12 @@ func TestGetOkexRestAPIKline(t *testing.T) {
 	okex.SetConfigure(Config{
 		Proxy: "SOCKS5:127.0.0.1:1080",
 	})
-	result := okex.GetKline("eth/usdt", KlinePeriod15Min, 100)
+	result := okex.GetKline("eth/usdt", KlinePeriod15Min, 500)
 
 	if len(result) != 0 {
 		log.Printf("共有%d条", len(result))
 
 		StrategyTrendTest(result, true, true)
-
-		outputs := checkBottomSupport(result)
-		for _, output := range outputs {
-			log.Printf("Log:%s", output)
-		}
 	}
 
 }
