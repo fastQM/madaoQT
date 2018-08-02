c44cb7f83d2cd14933f8247e89b83a4543592eee
maisuid <maisuid@gmail.com>
2018-08-02 17:04:51 +0800

    init oanda api

--- exchange/sina_ctp_test.go
+++ exchange/sina_ctp_test.go
@@ -8,14 +8,17 @@ import (
 )
 
 var instruments = []string{
-	"ma0",
-	"m0",
-	"ap0",
-	"cf0",
-	"bu0",
-	"sr0",
-	"zc0",
-	"fg0",
+	"RM1809",
+	"AP1810",
+	"rb1810",
+	"CF1901",
+	"m1809",
+	"j1809",
+	"bu1812",
+	"MA1809",
+	"SR1809",
+	"FG1809",
+	"hc1810",
 }
 
 func TestSinaCtp(t *testing.T) {
@@ -24,10 +27,10 @@ func TestSinaCtp(t *testing.T) {
 
 	for _, instrument := range instruments {
 
-		filename := instrument + "-1h"
+		filename := instrument + "-1day"
 		sina := new(SinaCTP)
 		if true {
-			klines = sina.GetKline(instrument, time.Now(), nil, KlinePeriod1Hour)
+			klines = sina.GetKline(instrument, time.Now(), nil, KlinePeriod1Day)
 			SaveHistory(filename, klines)
 			log.Printf("Init Done!!!")
 		} else {
@@ -48,12 +51,13 @@ func TestSinaCtp(t *testing.T) {
 		// result := StrategyTrendArea(klines, true, true)
 
 		// // for waveLimit := 0.1; waveLimit < 1; waveLimit += 0.1 {
-		for interval = 6; interval < 20; interval++ {
-			// SpliteSetWaveLimit(0.2)
-			result := CTPStrategyTrendSplit(klines, true, true, false)
-			msg := fmt.Sprintf("[%s]Offset:%.2f Result:%s", instrument, value, result)
-			logs = append(logs, msg)
-		}
+		// for interval = 6; interval < 20; interval++ {
+		// SpliteSetWaveLimit(0.2)
+		interval = 10
+		result := CTPStrategyTrendSplit(klines, true, true, false)
+		msg := fmt.Sprintf("[%s]Offset:%.2f Result:%s", instrument, value, result)
+		logs = append(logs, msg)
+		// }
 
 		// for interval := 1; interval < 100; interval++ {
 		// 	ChangeInterval(interval)
